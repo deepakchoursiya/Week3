a806e885cfd8d7b719e87d2be86263b8931604bc
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-29 00:46:22 +0530

    Program added to task scheduler

---
+++ src/Day01/circular_linkedlist/task_scheduler/Task.java
@@ -0,0 +1,19 @@
+package Day01.circular_linkedlist.task_scheduler;
+
+
+
+class Task {
+    int taskID;
+    String taskName;
+    int priority;
+    String dueDate;
+    Task next;
+
+    public Task(int taskID, String taskName, int priority, String dueDate) {
+        this.taskID = taskID;
+        this.taskName = taskName;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.next = null;
+    }
+}
