523fe800895b35472d4c4141186f642990d380d8
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-29 00:44:48 +0530

    Program added to online ticket reservation

---
+++ src/Day01/circular_linkedlist/online_ticket_reservation/Ticket.java
@@ -0,0 +1,21 @@
+package Day01.circular_linkedlist.online_ticket_reservation;
+
+
+
+class Ticket {
+    int ticketID;
+    String customerName;
+    String movieName;
+    String seatNumber;
+    String bookingTime;
+    Ticket next;
+
+    public Ticket(int ticketID, String customerName, String movieName, String seatNumber, String bookingTime) {
+        this.ticketID = ticketID;
+        this.customerName = customerName;
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.bookingTime = bookingTime;
+        this.next = null;
+    }
+}
